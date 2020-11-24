c9d557f4a4513a942bf99cb27385e2c7996d55b6
Olakunle E Neye <emmanuelneye@gmail.com>
2020-11-23 20:06:42 -0500

    Consuming api.kanye.rest API

--- KanyeWest/Program.cs
+++ KanyeWest/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
 
 namespace KanyeWest
 {
@@ -6,7 +8,18 @@ namespace KanyeWest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var client = new HttpClient();
+
+            string urlKanye = "https://api.kanye.rest";
+
+            var kanyeResponse = client.GetStringAsync(urlKanye).Result;
+
+            Console.WriteLine(kanyeResponse);
+
+            var kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+            Console.WriteLine();
+            Console.WriteLine(kanyeQuote);
+
         }
     }
 }

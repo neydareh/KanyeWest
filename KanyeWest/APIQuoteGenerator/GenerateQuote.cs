b5171e7a6d24ba82e3675cdf1ab9dea0dcd8ad6f
Olakunle E Neye <emmanuelneye@gmail.com>
2020-11-23 21:21:51 -0500

    Refactored the code to make it more efficient

---
+++ KanyeWest/APIQuoteGenerator/GenerateQuote.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json.Linq;
+using System.Net.Http;
+
+namespace KanyeWest.APIQuoteGenerator
+{
+    class GenerateQuote
+    {
+        private readonly HttpClient _client = new HttpClient();
+        public GenerateQuote(HttpClient client)
+        {
+            _client = client;
+        }
+        public string CallKanyeAPI()
+        {
+            string urlKanye = "https://api.kanye.rest";
+            var kanyeResponse = _client.GetStringAsync(urlKanye).Result;
+            var kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+            return kanyeQuote;
+        }
+        public string CallRonSwanson()
+        {
+            string urlRonSwanson = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+            var ronSwansonAPIResponse = _client.GetStringAsync(urlRonSwanson).Result;
+            var ronSwansonResponse = JArray.Parse(ronSwansonAPIResponse).ToString().Replace("[", "").Replace("]", "").Trim().Trim('\"');
+            return ronSwansonResponse;
+        }
+    }
+}
--- KanyeWest/Program.cs
+++ KanyeWest/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
-using KanyeWest.KanyeAPI;
-using KanyeWest.RonSwansonAPI;
+using KanyeWest.APIQuoteGenerator;
 using Lamar;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -11,19 +10,17 @@ namespace KanyeWest
         static void Main(string[] args)
         {
             var container = new Container(x => {
-                x.AddSingleton<RonSwanson>();
-                x.AddSingleton<Kanye>();
+                x.AddSingleton<GenerateQuote>();
             });
-
-            var ron = container.GetService<RonSwanson>();
-            var kanye = container.GetService<Kanye>();
+            var quotes = container.GetService<GenerateQuote>();
 
             Console.WriteLine("Weirdest conversations between Kanye West and Ron Swanson");
             Console.WriteLine();
+
             for (int i = 0; i < 5; i++)
             {
-                Console.WriteLine("Ron Swanson: " + ron.CallRonSwanson());
-                Console.WriteLine("Kanye West: " + kanye.CallKanyeAPI());
+                Console.WriteLine("Ron Swanson: " + quotes.CallRonSwanson());
+                Console.WriteLine("Kanye West: " + quotes.CallKanyeAPI());
                 Console.WriteLine();
             }
         }
